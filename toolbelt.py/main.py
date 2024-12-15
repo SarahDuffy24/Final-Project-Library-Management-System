8c84f4e4e68ff354e1c761c44721e87cb10c2b8d
SarahDuffy24 <sarah.duffy@maine.edu>
2024-12-14 21:15:34 -0500

    tool belt hw 6

---
+++ toolbelt.py/main.py
@@ -0,0 +1,16 @@
+# Sarah Duffy, Dec 14th 2024, tool belt
+
+
+'''
+numArray = input("Enter a List number: ")
+'''
+
+
+def countEvens(Array):
+    numevens = 0 
+    for x in (Array):
+        if x % 2 == 0:
+            numevens += 1
+    return numevens
+
+
